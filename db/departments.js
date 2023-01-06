7d2f77519ac3d9dfc8603d14aab19be3fc757007
John Kang <johnkang1121@gmail.com>
2023-01-05 16:32:33 -0800

    added logic to add, remove and veiw different deparments

---
+++ db/departments.js
@@ -0,0 +1,58 @@
+const db = require("./connection");
+const inquirer = require("inquirer");
+
+
+async function viewDepartments() {
+    try{
+        const departments = 
+            await db.query("SELECT * FROM department")
+                return departments
+    } catch (err){
+    console.log(err)
+    }    
+}
+
+async function addDepartment () {
+    try {
+        const departments = await viewDepartments();
+        const {
+            name,
+        } = await inquirer.prompt ([
+            {
+                type: 'input',
+                message: 'Which department should be added?',
+                name: 'name',
+            }
+        ])
+        await db.query(`INSERT into department (name) VALUES ("${name}")`)
+        const newDepartments = await viewDepartments();
+        return newDepartment       
+    }catch (err){
+        console.log(err);
+    }
+}
+
+async function removeDepartment() {
+    try {
+        const viewDepartments = await viewDepartments();
+        const {id} = await inquirer.prompt([
+            {
+                type: 'list',
+                message: 'which department should be deleted?',
+                name: 'id',
+                choices: viewAllDepartments.map((departments) => {
+                    return {
+                        name : department.name,
+                        value : department.id,
+                    };
+                }),
+            },
+        ]);
+        await db.query(`DELETE FROM department WHERE id = ${id}`);
+        return await viewDepartments();
+    }catch (err){
+        console.log(err);
+    }
+}
+
+module.exports = {viewDepartments, addDepartment, removeDepartment}
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-
 const { prompt } = require("inquirer");
 const db = require("./db/connection");
+const {veiwDepartments, addDepartments, removeDepartments} = require("./db/departments");
 
 const start = () => {
     console.log("Welcome to the Employee Manager!");
