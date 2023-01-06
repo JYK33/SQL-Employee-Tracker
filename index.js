e63ce3bfd937c6ecf1784c43039932c262e8f70a
John Kang <johnkang1121@gmail.com>
2023-01-05 17:25:23 -0800

    created iqnuirer prompts for the index.js

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const { prompt } = require("inquirer");
 const db = require("./db/connection");
-const {veiwDepartments, addDepartments, removeDepartments} = require("./db/departments");
+const {viewDepartments, addDepartment, removeDepartment} = require("./db/departments");
+const {viewEmployees, addEmployee, updateEmployee, removeEmployee} = require("./db/employees");
+const {viewRoles, addRole, removeRole} = require("./db/roles");
 
-const start = () => {
+const start = async (s) => {
     console.log("Welcome to the Employee Manager!");
+     const {choice} = await prompt ([
+        {
+            type: 'list',
+            message: 'What do you want to?',
+            name: 'choices',
+            choices: [
+                'View all departments',
+                'View all employees',
+                'View all roles',
+                'Add a department',
+                'Add a role',
+                'Add an employee',
+                'Update an employee',
+                'Remove an employee',
+                'Remove a department',
+                'Remove a role',
+                'Exit',
+            ]
+        }
+     ])
 }
 
 start();
